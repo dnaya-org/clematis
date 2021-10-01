c6975be7d747fe888354ef2b36b437cbf9578134
Filipe Verri <filipeverri@gmail.com>
2021-10-01 09:05:39 -0300

    Parsing arguments.

--- src/clematis/__main__.py
+++ src/clematis/__main__.py
@@ -1,10 +1,20 @@
+import argparse
 import sys
 
 from . import System
 from igraph import Graph
 
-assert len(sys.argv) == 2
-graph = Graph.Read(sys.argv[1])
+parser = argparse.ArgumentParser(
+    description='Complex Manufacturing Simulation')
+
+parser.add_argument('--input', type=str, required=True)
+parser.add_argument('--output', type=str, required=True)
+parser.add_argument('--seed', type=int, required=True)
+
+args = parser.parse_args()
+print(args)
+
+graph = Graph.Read(args.input) # XXX should we support stdin?
 
 print(graph)
 assert(graph.is_dag())
@@ -12,3 +22,11 @@ assert(graph.is_dag())
 print(graph.vs["production_rate"])
 print(graph.vs["failure_rate"])
 print(graph.es["buffer_size"])
+
+system = System(graph)
+
+if args.output == '-':
+    system.iterate(sys.stdout)
+else:
+    with open(args.output, "w") as f:
+        system.iterate(f)
--- src/clematis/system.py
+++ src/clematis/system.py
@@ -13,3 +13,15 @@ class System:
             property called `buffer_size` greater than 0.
         """
         assert network.is_dag()
+        assert frozenset(network.vertex_attributes()) >= frozenset(['production_rate', 'failure_rate', 'id'])
+        assert frozenset(network.edge_attributes()) >= frozenset(['buffer_size'])
+
+        self.network_ = network
+        self.time_ = 0
+
+    def iterate(self, output):
+
+        if self.time_ == 0:
+            output.write("time,vertex,state\n")
+
+        self.time_ = self.time_ + 1
